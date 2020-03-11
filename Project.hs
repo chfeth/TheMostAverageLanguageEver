7a0197206a86996d455b965a7aba423ea160fcba
Trevor-Jones <trevor.eric.j@gmail.com>
2020-03-10 21:01:31 -0700

    Added isbn example and fixed algorithm for leap year check

--- Project.hs
+++ Project.hs
@@ -25,6 +25,8 @@ data Cmd = PushT Type
          | Sub
          | Mul
          | Equ
+         | Mod
+         | Div
          | LTE Cmd
          | GTE Cmd
          | Let Var Prog Prog
@@ -32,7 +34,8 @@ data Cmd = PushT Type
          | Def Var Prog
          | Call Var
          | CallStack
-         | IfThen Prog Prog
+         | IfElse Prog Prog
+         | IfThen Prog
          | WhileLp Stmt
    deriving (Eq, Show)
 
@@ -94,12 +97,23 @@ cmd Mul e s       = case s of
                         ((I i) : (I j) : s') -> Just ((I (i * j)) : s')
                         _                                  -> Nothing
 cmd Equ e s       = case s of 
-                        ((I i) : (I j) : s')   -> Just ((B (i == j)) : s')
+                        ((I i) : (I j) : s') -> Just ((B (i == j)) : s')
                         ((B k) : (B l) : s') -> Just ((B (k == l)) : s')
                         _                                    -> Nothing
-cmd (IfThen t v ) env s  = case s of 
+cmd Mod e s       = case s of 
+                        ((I i) : (I j) : s') -> Just ((I (j `mod` i)) : s')
+                        _                    -> Nothing
+                        
+cmd Div e s       = case s of 
+                        ((I i) : (I j) : s') -> Just ((I (j `div` i)) : s')
+                        _                    -> Nothing
+cmd (IfElse t v ) env s  = case s of 
                                  ((B True) : s')  -> prog t env s'
                                  ((B False) : s') -> prog v env s'
+                                 _                        -> Nothing
+cmd (IfThen t) env s  = case s of 
+                                 ((B True) : s')  -> prog t env s'
+                                 ((B False): s')  -> Just s'
                                  _                        -> Nothing
 cmd (Let x b v) env s    = case prog b env s of 
                                  Just (i : s') -> prog v (set x i env) s'
@@ -113,7 +127,7 @@ cmd (Call x) env s       = case get x env of
 cmd CallStack env s      = case s of
                                  ((P p): s') -> prog p env s'
                                  _          -> Nothing
-cmd (WhileLp x) env s      = stmt x s
+cmd (WhileLp x) env s      = stmt x env s
 
 --EXECUTE LIST OF CMDS
 prog :: Prog -> Env -> Domain
@@ -140,40 +154,62 @@ set x i m y = if y == x then Just i else m y -- instead of m y could be get y m
 
 
 --WHILE LOOPS
-stmt :: Stmt -> Domain
-stmt (Set e)     s = cmd e empty s
-stmt (While c b)  s = case cmd c empty s of
-                           Just ((B True) : s') -> case (stmt b s') of 
-                                                      Just t -> stmt (While c b) t
+stmt :: Stmt -> Env -> Domain
+stmt (Set e) env s = cmd e env s
+stmt (While c b) env  s = case cmd c env s of
+                           Just ((B True) : s') -> case (stmt b env s') of 
+                                                      Just t -> stmt (While c b) env t
                                                       _      -> Just s
                            Just ((B False) : s') -> Just s
                            _                     -> Nothing
-stmt (Begin ss)  s = stmts ss s
+stmt (Begin ss) env s = stmts ss env s
 
-stmts :: [Stmt] -> Domain
-stmts [] s = Just s
-stmts (x : xs) s = case stmt x s of
-                         Just t -> stmts xs t
+stmts :: [Stmt] -> Env -> Domain
+stmts [] env s = Just s
+stmts (x : xs) env s = case stmt x env s of
+                         Just t -> stmts xs env t
                          _      -> Nothing
 
 
 nott :: Cmd
-nott = IfThen [PushT (B False)] [PushT (B True)]
-
-
+nott = IfElse [PushT (B False)] [PushT (B True)]
 
 --EXAMPLE OF GOOD PROGRAMS
 
 goodExample1 :: Prog
-goodExample1 = [(WhileLp (Begin [Set (PushT (I 2020)),While (GTE (PushT (I 100))) (Begin [(Set (PushT (I 4))), (Set (Sub))])])), PushT (I 0), Equ, (IfThen [PushT (S "Leap Year")] [PushT (S "Non-Leap Year")])]
+goodExample1 = [PushT (I 1900), Dup, (WhileLp (While (GTE (PushT (I 1))) (Begin [(Set (PushT (I 4))), (Set (Sub))]))), -- push year and while loop sub 4 to check if year is divisible by 4
+                -- this next line checks the output of the while loop, and checks if the year is divisible by 100, and if it is if it is also divisible by 400, and pushes the correct leapyear/non-leapyear text to stack
+                Swap, Dup, Rot,                           -- duplicate year so we have it for later checks
+                PushT (I 0), Equ,                         -- check outcome from previous loop to see if year is divisible by 4
+                -- if it is divisible by 4, then also check 100 and 400, as 100 only years are not leap years, but 400 years are leap years.
+                (IfElse [PushT (I 100), Mod, PushT (I 0), Equ, IfElse [PushT (I 400), Mod, PushT(I 0), Equ, IfElse [PushT (S "Leap Year")] [PushT (S "Non-Leap Year")]] [PushT (S "Leap Year")]] [PushT (S "Non-Leap Year")])]
 -- THE ABOVE CHECKS WHETHER A GIVEN YEAR IS A LEAP YEAR. REPLACE "1900" WITH THE YEAR YOU WANT TO CHECK
 
-funcExample1 :: Prog
-funcExample1 = [Def "Add5Flip" [(PushT (I 5)), Add, (PushT (I (-1))), Mul], (PushT(I (-335))), Call "Add5Flip"]
--- THE ABOVE DEFINES A FUNCTION THAT ADDS 5 AND FLIPS THE SIGN, THEN PUSHES -335 AND RUNS IT, RESULTING IN 330
+-- This example takes a 13 digit isbn and checks it's check bit to make sure it is a valid isbn number
+-- https://isbn-information.com/check-digit-for-the-13-digit-isbn.html
+-- Example isbn to use:
+-- 9783161484100 - valid
+-- 9781861972712 - valid
+-- 9781681972712 - invalid
+-- 9781861973712 - invalid
+-- top value of stack is either true or false based on validty of isbn after running
+goodExample2 :: Prog
+goodExample2 = [(Def "SplitInt" [Dup, (PushT (I 10)), Div, Swap, (PushT (I 10)), Mod, Swap]),    -- function to split integer into digits
+                (PushT (I 9783161484100)),                                                       -- isbn we want to use REMOVE THIS LINE IF YOU WANT TO PASS IN YOUR OWN ISBN ON THE STACK
+                (Call "SplitInt"), (PushT (I (-1))), Swap,                                       -- do the  first call and push a -1 so we know when the first 12 digits end
+                (WhileLp (While (GTE (PushT (I 10))) (Begin [Set(Call "SplitInt")]))),           -- while loop to call SplitInt until isbn number is split into single digits
+                (Def "FlipOneThree" [(GTE (PushT (I 3))), IfElse [Drop, PushT (I 1)] [Drop, PushT (I 3)]]),    -- function that swaps the first number on the stack between 1 or 3
+                (PushT (I 1)), (PushT (I 0)), Rot,                                               -- setup for summing number, 1 is what we multiply by, 0 is the starting sum
+                -- while loop that loops through and multiplies the first num by 1, then the next num by 3, and adds them, then multiplies the third num by 1, and adds to sum.
+                -- it continues through all numbers, alternating between 1 and 3 by pushing the FlipOneThree function to the stack and calling it, until it sums all 12 digits.
+                (WhileLp (While (GTE (PushT (I 0))) (Begin [Set(Swap), Set(Dup), Set(Rot), Set(Mul), Set(Swap), Set(Ref "FlipOneThree"), Set(CallStack), Set(Rot), Set(Add), Set(Rot)]))),
+                -- drop the 1or3 counter and -1 marker, then mod 10 to get remainder, and subtract from 10 if not 0, then compare to 13th check bit in isbn
+                Drop, Drop, (PushT (I 10)), Mod, (GTE (PushT (I 1))), IfThen [PushT (I 10), Swap, Sub], Equ]
 
-funcExample2 :: Prog
-funcExample2 = [(PushT (I 25)), (PushT(P [(PushT (I 5)), Add, (PushT (I (-1))), Mul])), CallStack]
+
+funcExample1 :: Prog
+funcExample1 = [(PushT (I 25)), (PushT(P [(PushT (I 5)), Add, (PushT (I (-1))), Mul])), CallStack]
+-- THE ABOVE PUSHES 25, THEN DEFINES A FUNCTION THAT ADDS 5 AND FLIPS THE SIGN, AND PUSHES THAT FUNCTION TO THE STACK. IT RUNS IT OFF THE STACK, RESULTING IN -30
 
 --EXAMPLE OF BAD PROGRAMS
 badExample1 :: Prog
@@ -181,5 +217,3 @@ badExample1 = [(PushT (S "test")), (PushT (I 5)), Add]
 
 badExample2 :: Prog
 badExample2 = [PushT (I 5), Add]
-
-
--- README.txt
+++ README.txt
@@ -11,4 +11,9 @@ Run in GHCi, load Project.hs.
 
 To run goodExample 1, execute "prog goodExample1 empty []" in GHCi. Note that the result should be Just [S "Non-Leap Year"], because the coded year is 1900. To test a different year that may be a leap year, change "1900" in the code to the desired year.
 
+To run goodExample2, execute "prog goodExample2 empty []" in GHCi. It will check an isbn number for validity using the check digit and its algorithm.
+You can change the isbn it is checking in the code, and some samples are there in comments. The top of the stack will be either true or false based on the validity.
+You can also comment out the line pushing the isbn to the stack in code, and run it like "prog goodExample2 empty [I 9783161484100]" to pass it an isbn to run
+More info on isbn can be found here: https://isbn-information.com/check-digit-for-the-13-digit-isbn.html
+
 To run badExample 1 & badExample2, execute "prog badExample1 empty []" and "prog badExample2 empty []" respectively in GHCi. The expected output for both is "Nothing" where badExample1 causes a type error and badExample2 causes an underflow error. 
